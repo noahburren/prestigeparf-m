a7dd582df0a25a33fde26137938d983995931c2d
LeviFuchs <lefy1407outlook.de>
2024-10-25 16:18:56 +0200

    datenschutz

--- src/Datenschutz.js
+++ src/Datenschutz.js
@@ -3,10 +3,38 @@ import React from "react";
 
 const Datenschutz = () => {
   return (
-    <div>
+    <div className="privacy-policy">
       <h1>Datenschutz</h1>
-      <p>Hier ist die Datenschutzerklärung.</p>
-      {/* Füge hier den Inhalt der Datenschutzseite hinzu */}
+      <h2>Datenschutzerklärung</h2>
+      <p>
+        Erhebung, Verarbeitung und Nutzung personenbezogener Daten Bei einem
+        Besuch dieser Website erheben wir keine personenbezogenen Daten von
+        Ihnen. Personenbezogene Daten werden nur erhoben, wenn Sie uns diese
+        über unser Kontaktformular mitteilen. Diese Daten werden ausschließlich
+        zur Beantwortung Ihres Anliegens genutzt und danach wieder gelöscht. .
+      </p>
+      <h2>Verwendung von eigenen Cookies</h2>
+      <p>
+        Beim Aufruf bestimmter Seiten wird ein sogenanntes «Session-Cookie»
+        gesetzt. Hierbei handelt es sich um eine kleine Textdatei, die nach Ende
+        der Browser-Sitzung automatisch wieder von Ihrem Computer gelöscht wird.
+        Diese Datei dient ausschließlich dazu, bestimmte Applikationen nutzen zu
+        können. Sie können bei den meisten Webbrowsern die Einstellungen so
+        ändern, dass Ihr Browser neue Cookies nicht akzeptiert oder Sie können
+        erhaltene Cookies löschen lassen. Wie dies konkret für Ihren Browser
+        funktioniert, kann in der Regel über dessen Hilfe-Funktion in Erfahrung
+        gebracht werden.
+      </p>
+      <h2>Ihre Rechte</h2>
+      <p>
+        Ihnen stehen grundsätzlich die Rechte auf Auskunft, Berichtigung,
+        Löschung, Einschränkung, Daten, Übertragbarkeit, Widerruf und
+        Widerspruch zu. Wenn Sie glauben, dass die Verarbeitung Ihrer Daten
+        gegen das Datenschutzrecht verstößt oder Ihre datenschutzrechtlichen
+        Ansprüche sonst in einer Weise verletzt worden sind, können Sie sich bei
+        der Aufsichtsbehörde beschweren. Sie erreichen uns unter
+        folgenden Kontaktdaten:
+      </p>
     </div>
   );
 };
