224ea12056b51cc7e9ac516831e9e3558eedb6cb
aj-kry <ajnakryeziu09@gmail.com>
2024-10-25 11:02:27 +0200

    Impressum

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import Datenschutz from "./Datenschutz";
 import Contact from "./Contact";
 import Parfums from "./Parfums";
 import ParfumInfo from "./ParfumInfo";
+import Impressum from "./Impressum";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/datenschutz" element={<Datenschutz />} />
+      <Route path="/impressum" element={<Impressum/>} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/parfums" element={<Parfums />} />
       <Route path="/parfuminfo" element={<ParfumInfo />} />
---
+++ src/Impressum.js
@@ -0,0 +1,14 @@
+// src/Datenschutz.js
+import React from "react";
+
+const Impressum = () => {
+  return (
+    <div>
+      <h1>Impressum</h1>
+      <p>Hier ist das Impressum.</p>
+      {/* Füge hier den Inhalt des Impressums hinzu */}
+    </div>
+  );
+};
+
+export default Impressum;
